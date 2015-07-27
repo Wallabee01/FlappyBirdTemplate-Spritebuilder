35d53e4599ce3adea14d49b17687ba3d331cdae9
Wallabee01 <phrozenfire01@gmail.com>
2015-07-26 19:40:37 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,6 +15,9 @@
     // put update code here
 }
 
-// put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+	// This will get called every time the player touches the screen
+	[character flap];
+}
 
 @end
